e6a44ead476bcd17907c2d745796eb3222bf90ef
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2021-03-20 12:52:12 -0400

    heroku push

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <Box bg={useColorModeValue('white', 'gray.800')} id="footer-wrap">
       <Center h="10px" pt="5">
-        <Text fontSize="xs">Made by Andrea Crego</Text>
+        <Text fontSize="xs">Made by Andrea Crego 2021</Text>
       </Center>
     </Box>
   )
